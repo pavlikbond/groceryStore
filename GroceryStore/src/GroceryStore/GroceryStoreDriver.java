76a0fff2921e57555fe9ef78d8d490e212a4b209
jacob <Jacob@10.0.0.12>
2021-10-26 00:05:18 -0500

    Added partially implemented checkout function Added getter for product and transaction in GroceryStore Added tests for addProduct and checkout in Driver

--- GroceryStore/src/GroceryStore/GroceryStoreDriver.java
+++ GroceryStore/src/GroceryStore/GroceryStoreDriver.java
@@ -12,6 +12,29 @@ public class GroceryStoreDriver {
 		store.enrollMember("Pavel Bondarenko", "123 Main St. Chaska, MN 55318", "952 123-4567", date, 100.00);
 		System.out.println(store.removeMember(0));
 		System.out.println(store.removeMember(1));
+
+		// Testing addProduct, checkout
+		store.addProduct("a", 20, 10.5, 5);
+		store.addProduct("b", 20, 10.02, 5);
+		store.addProduct("c", 20, 10.11, 5);
+		store.addProduct("d", 20, 10, 5);
+		store.addProduct("e", 20, 10, 5);
+
+		store.checkOutItems(0, date);
+		System.out.println("Member: " + store.getTransaction(0).getMemberID());
+
+		for (Product product : store.getTransaction(0).getProductList()) {
+			System.out.println(product.getName());
+		}
+
+		System.out.println("Product a stock: " + store.getProduct(0).getCurrentStock());
+		System.out.println("Product b stock: " + store.getProduct(1).getCurrentStock());
+		System.out.println("Product c stock: " + store.getProduct(2).getCurrentStock());
+		System.out.println("Product d stock: " + store.getProduct(3).getCurrentStock());
+		System.out.println("Product e stock: " + store.getProduct(4).getCurrentStock());
+
+		System.out.println("Transaction Total: " + store.getTransaction(0).getTotal());
+
 	}
 
 }
